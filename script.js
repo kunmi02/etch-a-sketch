8c49e74f55dcc72c4e9ae73d32a207fba908bf76
Akinkunmi02@bitbucket.com <Akinkunmi02@bitbucket.com>
2020-10-09 15:18:33 +0100

    etch-a-sketch commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+const container = document.getElementById("container");
+
+
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    // cell.innerText = (c + 1);
+    container.appendChild(cell).className = "grid-item";
+    cell.addEventListener('mouseover', () => {
+      cell.classList.add('hover');
+    })
+  };
+};
+
+makeRows(16, 16);
+
+const reset = document.getElementById('reset');
+reset.addEventListener('click', function (e) {
+  location.reload()
+});
